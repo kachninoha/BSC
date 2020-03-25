d879aa4a5fc866cef2d6232f3968e1b8d0b8db16
Petr Menšík <petr.mensik@embedit.cz>
2020-03-25 02:55:36 +0100

    init

---
+++ src/main/java/com/bsc/currency/paymentracker/PaymentTrackerApplication.java
@@ -0,0 +1,123 @@
+package com.bsc.currency.paymentracker;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.math.BigDecimal;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+public class PaymentTrackerApplication {
+
+    private static final String ERROR_MESSAGE = "Error: incorrect input format. " +
+            "It should be in currency code (3 upper letters) and currency amount divided by whitespace!!! InputLine: ";
+
+    private static final String CURRENCY_PATTERN = "[A-Z]{3}";
+    private static final String EXIT_PATTERN = "exit";
+
+    private static final long DEFAULT_PERIOD = new Long(60000); //60 seconds
+
+
+    public static void main(String[] args) {
+
+        final ConcurrentMap<String, BigDecimal> currencyMap = new ConcurrentHashMap<>(200);
+
+        final Timer timer = new Timer();
+        Long period = DEFAULT_PERIOD;
+        String fileName = null;
+
+        if(args.length == 1) {
+            if (isLong(args[0])) {
+                period = Long.parseLong(args[0]);
+            } else {
+                fileName = args[0];
+            }
+        }
+        if (args.length == 2) {
+            fileName = args[0];
+            if (isLong(args[1])) {
+                period = Long.parseLong(args[1]);
+            }
+        }
+
+        final TimerTask printTask = preparePrintTask(currencyMap);
+        timer.schedule(printTask, 0, period);
+
+        if (fileName != null) {
+            try (Scanner sc = prepareFileScanner(fileName)) {
+                doReadScannerToMap(sc, currencyMap);
+            }
+        }
+
+        try (Scanner sc = new Scanner(System.in)) {
+            doReadScannerToMap(sc, currencyMap);
+        }
+
+        printTask.cancel();
+        timer.cancel();
+    }
+
+    /**
+     * @param sc          scanner to read from
+     * @param currencyMap ConcurrentMap to write to
+     */
+    private static void doReadScannerToMap(final Scanner sc, final ConcurrentMap<String, BigDecimal> currencyMap) {
+        while (sc.hasNextLine()) {
+            if (sc.hasNext(EXIT_PATTERN)) {
+                break;
+            }
+            final String currencyCode = sc.hasNext(CURRENCY_PATTERN) ? sc.next(CURRENCY_PATTERN) : null;
+            final BigDecimal currencyValue = sc.hasNextBigDecimal() ? sc.nextBigDecimal() : null;
+            if (currencyCode != null && currencyValue != null) {
+                currencyMap.merge(currencyCode, currencyValue, BigDecimal::add);
+            } else {
+                System.out.println(ERROR_MESSAGE + (sc.hasNext() ? sc.next() : ""));
+            }
+        }
+    }
+
+    /**
+     * prepare task for scheduling regularly print results from the given concurent hash map
+     *
+     * @param currencyMap ConcurrentMap to write to
+     */
+    private static TimerTask preparePrintTask(final ConcurrentMap<String, BigDecimal> currencyMap) {
+        Objects.requireNonNull(currencyMap);
+        final TimerTask printTask = new TimerTask() {
+            @Override
+            public void run() {
+                currencyMap.forEach((key, value) -> {
+                    if (!BigDecimal.ZERO.equals(value)) {
+                        System.out.println(key + " " + value);
+                    }
+                });
+            }
+        };
+        return printTask;
+    }
+
+    /**
+     * Creates Scanner over the given file, throws RuntimeException if File isnt correct or not exist
+     *
+     * @param filePath path to file
+     * @return Scanner created from file
+     */
+    private static Scanner prepareFileScanner(final String filePath) {
+        try {
+            return new Scanner(new File(filePath), StandardCharsets.UTF_8.toString());
+        } catch (FileNotFoundException e) {
+            throw new IllegalArgumentException("first command line argument has to be valid path to valid and openable file", e);
+        }
+    }
+
+    private static boolean isLong(final String arg) {
+        try {
+            Long.parseLong(arg);
+            return true;
+        } catch (NumberFormatException nfe) {
+            return false;
+        }
+    }
+}
+
---
+++ src/test/java/com/bsc/currency/paymentracker/PaymentTrackerApplicationTest.java
@@ -0,0 +1,139 @@
+package com.bsc.currency.paymentracker;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class PaymentTrackerApplicationTest {
+
+    public static final String ERROR_MESSAGE = "Error: incorrect input format. " +
+            "It should be in currency code (3 upper letters) and currency amount divided by whitespace!!!";
+
+    private final InputStream systemIn = System.in;
+    private final PrintStream systemOut = System.out;
+
+    private ByteArrayInputStream testIn;
+    private ByteArrayOutputStream testOut;
+
+    final String okfile = "src/test/resources/testokfile.txt";
+    final String badfile = "src/test/resources/badfile.txt";
+
+    final String[] oneSecondPeriod = new String[] {"500"};
+
+    @Before
+    public void setUpOutput() {
+        testOut = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(testOut));
+    }
+
+    @After
+    public void restoreSystemInputOutput() throws IOException {
+        System.setIn(systemIn);
+        System.setOut(systemOut);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testFileWrongName() {
+        PaymentTrackerApplication.main(new String[]{"! =", "100"});
+    }
+
+    //@Test
+    public void testFileWrongFirstLineFile() throws InterruptedException {
+        setUpOutput();
+        waitAndExit();
+        runPaymentTracker(new String[]{badfile, "500"});
+        PaymentTrackerApplication.main(new String[]{badfile, "500"});
+        assertEquals(ERROR_MESSAGE, getOutput().trim());
+    }
+
+    @Test
+    public void testoutput1() {
+        provideInput("ajajaj");
+        PaymentTrackerApplication.main(new String[]{"500"});
+        provideExit();
+        assertTrue(getOutput().trim().startsWith(ERROR_MESSAGE));
+    }
+
+    //@Test
+    public void testoutput2() throws InterruptedException {
+        provideInput("USD 10\n");
+        PaymentTrackerApplication.main(new String[]{"300"});
+        waitAndExit();
+    }
+
+    @Test
+    public void testoutput3() {
+        provideInput("USD 1a0");
+        PaymentTrackerApplication.main(new String[]{"500"});
+        provideExit();
+        assertTrue(getOutput().trim().startsWith(ERROR_MESSAGE));
+    }
+
+    @Test
+    public void testoutput4() throws InterruptedException {
+        provideInput("USD 10 5");
+        PaymentTrackerApplication.main(new String[]{"300"});
+        provideExit();
+        assertTrue(getOutput().trim().startsWith(ERROR_MESSAGE));
+    }
+
+    //@Test
+    public void testoutputOK() throws InterruptedException, IOException {
+        restoreSystemInputOutput();
+        setUpOutput();
+        provideInput("USD 10");
+        PaymentTrackerApplication.main(new String[]{"300"});
+        //runPaymentTracker(new String[]{"300"});
+        waitAndExit();
+        assertEquals("USD 10", getOutput().trim());
+    }
+
+    private void provideInput(String data) {
+        ByteArrayInputStream stream = null;
+        try {
+            stream = new ByteArrayInputStream(data.getBytes(StandardCharsets.UTF_8.toString()));
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException(e);
+        }
+        testIn = stream;
+        System.setIn(testIn);
+    }
+
+    private void runPaymentTracker(String[] args) throws InterruptedException {
+        Thread t = new Thread(() -> PaymentTrackerApplication.main(args));
+        t.start();
+        t.join();
+    }
+
+    private void provideExit() {
+        provideInput("exit");
+    }
+
+    private void waitAndExit() {
+        try {
+            TimeUnit.MILLISECONDS.sleep(1180);
+            provideExit();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private String getOutput() {
+        final String out;
+        try {
+            out = testOut.toString(StandardCharsets.UTF_8.toString());
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException(e);
+        }
+        systemOut.println(out);
+        return testOut.toString();
+    }
+
+}
